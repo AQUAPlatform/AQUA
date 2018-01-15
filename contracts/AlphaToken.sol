42f48e654a8e4a966157bdf98dd0f7cd377afc78
Excavator Lu <excavator.lu@gmail.com>
2018-01-15 11:00:22 +0800

    use BurnableToken

--- contracts/AlphaToken.sol
+++ contracts/AlphaToken.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.4.17;
 import "zeppelin-solidity/contracts/token/PausableToken.sol";
+import "zeppelin-solidity/contracts/token/BurnableToken.sol";
 
 
 //1 ETH:1000000 ALPHA, 1 Wei:1000 unit
-contract AlphaToken is PausableToken {
+contract AlphaToken is PausableToken, BurnableToken {
   string public name = "Alpha Token";
   string public symbol = "ALPHA";
   uint public decimals = 15;
