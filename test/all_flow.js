26d3e6f3efe4fa8ba2d1ebf2ac300e4870c207d0
Excavator Lu <excavator.lu@gmail.com>
2018-01-15 20:54:05 +0800

    all_flow case1 success

--- test/all_flow.js
+++ test/all_flow.js
@@ -20,7 +20,7 @@ contract('ALL', function(accounts) {
         const startTime = new web3.BigNumber(web3.eth.getBlock(web3.eth.blockNumber).timestamp + 1); // one second in the future
         const endTime = startTime.plus(86400 * 20); // 20 days
         const rate = new web3.BigNumber(1000000);
-        const minInvest = web3.toWei(1, "ether");
+        const minInvest = web3.toWei(100, "ether");
         const maxInvest = web3.toWei(5000, "ether");
         const wallet = accounts[0];
         const token = await AlphaToken.new();
@@ -29,20 +29,63 @@ contract('ALL', function(accounts) {
         }
         const owner = accounts[0];
         const manager = accounts[1];
+
         const investor1 = accounts[2];
+        //users below not in white list
         const investor2 = accounts[3];
+
+        //developers 
         const developer1 = accounts[4];
         const developer2 = accounts[5];
 
-        //developer vesting
-        const vesting_ctl = await VestingCtrl.new(token.address, accounts[0], {from: accounts[1]});
-        const nowTime = new web3.BigNumber(web3.eth.getBlock(web3.eth.blockNumber).timestamp);
+        async function case_01() {
+            //developer vesting
+            const vesting_ctl = await VestingCtrl.new(token.address, accounts[0], {from: manager});
+            const nowTime = new web3.BigNumber(web3.eth.getBlock(web3.eth.blockNumber).timestamp);
+            const TOTAL_SUPPLY = await token.INITIAL_SUPPLY.call();
+            const VESTIING_SUPPLY = TOTAL_SUPPLY.div(5);
+            const PSALE_SUPPLY = TOTAL_SUPPLY.mul(40).div(100);
+            await token.approve(vesting_ctl.address, VESTIING_SUPPLY, {from: owner});
+            await vesting_ctl.newVesting(manager, nowTime, 5, 10, VESTIING_SUPPLY.div(4), {from: manager});
+            await vesting_ctl.newVesting(developer1, nowTime, 5, 10, VESTIING_SUPPLY.div(4), {from: manager});
+            await vesting_ctl.newVesting(developer2, nowTime, 5, 10, VESTIING_SUPPLY.div(2), {from: manager});
 
-        //private sale
-        const tokenSale = await AlphaTokenSale.new(startTime, endTime, rate, wallet, minInvest, maxInvest, token.address, web3.eth.accounts.slice(0, 3));
+            console.log("VESTING DONE");
 
-        //buyback
-        const buyback = await Buyback.new(token.address, wallet, rate, {from: accounts[1]});
+            //private sale
+            const tokenSale = await AlphaTokenSale.new(startTime, endTime, rate, owner, minInvest, maxInvest, token.address, web3.eth.accounts.slice(0, 3), {from: manager});
+            await token.approve(tokenSale.address, PSALE_SUPPLY);
 
+            await tokenSale.sendTransaction({from: investor1, value: web3.toWei(10000, "ether")});
+
+            await tokenSale.pause({from: manager});
+
+            console.log("SALE DONE");
+
+            //buyback
+            const buyback = await Buyback.new(token.address, wallet, rate.div(2), {from: manager});
+            await buyback.sendTransaction({from: owner, value: web3.toWei(10000, "ether")});
+            
+            const investor1Balance = await token.balanceOf.call(investor1, {from: investor1});
+            await token.approve(buyback.address, investor1Balance, {from: investor1});
+            await buyback.claim({from: investor1});
+
+            assert.equal(getBalance(buyback.address), 0, "no balance left");
+
+            await buyback.close({from: manager})
+            console.log("BUYBACK DONE");
+
+            //wait vesting done
+            await sleep(10000);
+            await vesting_ctl.release({from: manager});
+            await vesting_ctl.release({from: developer1});
+            await vesting_ctl.release({from: developer2});
+
+            assert.ok((await token.balanceOf.call(manager)).equals(VESTIING_SUPPLY.div(4)), "manager supply match");
+            assert.ok((await token.balanceOf.call(developer1)).equals(VESTIING_SUPPLY.div(4)), "developer1 supply match");
+            assert.ok((await token.balanceOf.call(developer2)).equals(VESTIING_SUPPLY.div(2)), "developer2 supply match");
+        }
+
+        await case_01();
     });
 });
