a63fe8254a9de428c192e3f182f7a2c1696cfcb2
Excavator Lu <excavator.lu@gmail.com>
2018-05-22 18:55:00 +0800

    using local net

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Smart Contract for AQUA-Platform
 ### Deploy
 
 ```bash
-MNEMONIC=$YOUR_MNEMONIC APIKEY=$YOUR_INFURA_APIKEY truffle migrate --reset --network=rinkeby
+truffle migrate --reset --network=mainnet
 ```
--- truffle.js
+++ truffle.js
@@ -1,7 +1,5 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
-const HDWalletProvider = require('truffle-hdwallet-provider')
-const fs = require('fs')
 
 const mnemonic = process.env.MNEMONIC
 const apikey = process.env.APIKEY
@@ -14,21 +12,10 @@ module.exports = {
       network_id: '*', // Match any network id,
       gas: 4000000,
     },
-    kovan: {
-      provider: new HDWalletProvider(mnemonic, 'https://kovan.infura.io/'+apikey),
-      network_id: '*',
-      gas: 4500000,
-      gasPrice: 25000000000
-    },
-    rinkeby: {
-      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/'+apikey),
-      network_id: '*',
-      gas: 4500000,
-      gasPrice: 25000000000
-    },
     mainnet: {
-      provider: new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/'+apikey),
-      network_id: '*',
+      host: '192.168.11.112',
+      port: 8545,
+      network_id: '*', // Match any network id,
       gas: 4500000,
       gasPrice: 25000000000
     }
